528126eba711cefeab52f4a52ffed068a2c13710
Daniel TIllero <danieltillero67@gmail.com>
2020-10-18 00:18:18 -0400

    Routing image paths

--- server/index.js
+++ server/index.js
@@ -1,9 +1,25 @@
 const http = require("http");
+const { readFile } = require("fs").promises;
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("I have items.");
+const server = http.createServer(async (req, res) => {
+    try {
+        if (req.url.startsWith("/images")) {
+            const imageFilePath = "./assets" + req.url;
+            const imageFileContents = await readFile(imageFilePath);
+            res.statusCode = 200;
+            const extension = path.extname(imageFilePath).substring(1);
+            res.setHeader("Content-Type", `image/${extension}`);
+            res.end(imageFileContents);
+            return;
+        }
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("I have items.");
+    } catch (err) {
+        console.log("ERROR: ", err);
+    }
 });
 
 const port = 8081;
